5b273c1e98c17dbaa9500f1eebf00249883e105d
saikalyan <bandepallis@gmail.com>
2024-11-02 11:41:27 +0530

    turning it into a microservice

---
+++ back-end/services/comment-reaction-service/comment-reaction-service/src/main/java/com/microservice/comment_reaction_service/CommentReactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.comment_reaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentReactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentReactionServiceApplication.class, args);
+	}
+
+}
---
+++ back-end/services/comment-reaction-service/comment-reaction-service/src/test/java/com/microservice/comment_reaction_service/CommentReactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.comment_reaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentReactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
